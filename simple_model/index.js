6711e9523698225d14c513c017b075bbab9e624f
julianbei <julian@contentpass.de>
2017-09-12 19:49:47 +0200

    Extract simulations in an own file

--- simple_model/index.js
+++ simple_model/index.js
@@ -1,34 +1,4 @@
-function simulateNumericDecision(fleet1, fleet2){
-  if (fleet1.ships > fleet2.ships) return fleet1;
-  if (fleet1.ships < fleet2.ships) return fleet2;
-  return null;
-}
-
-function simulateNumericDestruction(fleet1, fleet2){
-  // simulate destruction of ships
-  let ships = fleet1.ships - fleet2.ships;
-  // clone parameter objects for immutability
-  let combattant1 = {name: fleet1.name, ships};
-  let combattant2 = {name: fleet2.name, ships: ships*-1};
-
-  if (ships > 0) {
-    combattant2.ships = 0;
-    return {
-      winner: combattant1,
-      looser: combattant2
-    };
-  } else {
-    combattant1.ships = 0;
-    return {
-      winner: combattant2,
-      looser: combattant1
-    };
-  }
-  return {
-    draw1: combattant1,
-    draw2: combattant2
-  };
-}
+const simulate = require('./simulations');
 
 // Initialize test fleets
 const orion = {
@@ -49,11 +19,11 @@ const hasle = {
 // Simulations:
 
 //simulateNumericDecision
-console.log('simulateNumericDecision fight: 1\nwinner:', simulateNumericDecision(orion, commonwealth));
-console.log('simulateNumericDecision fight: 2\nwinner:', simulateNumericDecision(commonwealth, orion));
-console.log('simulateNumericDecision fight: 3\nwinner:', simulateNumericDecision(hasle, commonwealth));
+console.log('numericDecision fight: 1\nwinner:', simulate.numericDecision(orion, commonwealth));
+console.log('numericDecision fight: 2\nwinner:', simulate.numericDecision(commonwealth, orion));
+console.log('numericDecision fight: 3\nwinner:', simulate.numericDecision(hasle, commonwealth));
 
 //simulateNumericDestruction
-console.log('simulateNumericDestruction fight: 1\nwinner:', simulateNumericDestruction(orion, commonwealth));
-console.log('simulateNumericDestruction fight: 2\nwinner:', simulateNumericDestruction(commonwealth, orion));
-console.log('simulateNumericDestruction fight: 3\nwinner:', simulateNumericDestruction(hasle, commonwealth));
+console.log('numericDestruction fight: 1\nwinner:', simulate.numericDestruction(orion, commonwealth));
+console.log('numericDestruction fight: 2\nwinner:', simulate.numericDestruction(commonwealth, orion));
+console.log('numericDestruction fight: 3\nwinner:', simulate.numericDestruction(hasle, commonwealth));
