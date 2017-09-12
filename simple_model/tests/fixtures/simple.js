8b21dfaff33da005c8c832c57de930ab0076e57b
julianbei <julian@contentpass.de>
2017-09-12 23:29:59 +0200

    Extract test fleets in fixtures

---
+++ simple_model/tests/fixtures/simple.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'orion': {
+        name: 'orion',
+        ships: 295
+    },
+    'commonwealth': {
+        name: 'commonwealth',
+        ships: 300
+    },
+    'hasle': {
+        name: 'hasle',
+        ships: 300
+    }
+};
--- simple_model/tests/simpleTests.js
+++ simple_model/tests/simpleTests.js
@@ -1,29 +1,15 @@
 const expect = require('chai').expect;
 const simulate = require('./../src/index').simulate;
-
-const orion = {
-    name: 'orion',
-    ships: 295
-};
-
-const commonwealth = {
-    name: 'commonwealth',
-    ships: 300
-};
-
-const hasle = {
-    name: 'hasle',
-    ships: 300
-};
+const fixtures = require('./fixtures/simple');
 
 describe('simulations', function() {
     describe('numericDecision', function() {
         it('should find the winner of two unequal fleets', () => {
-            expect(simulate.numericDecision(orion, commonwealth)).to.eql(commonwealth);
-            expect(simulate.numericDecision(commonwealth, orion)).to.eql(commonwealth);
+            expect(simulate.numericDecision(fixtures.orion, fixtures.commonwealth)).to.eql(fixtures.commonwealth);
+            expect(simulate.numericDecision(fixtures.commonwealth, fixtures.orion)).to.eql(fixtures.commonwealth);
         });
         it('should identify equal fleets as a draw', () => {
-            expect(simulate.numericDecision(hasle, commonwealth)).to.be.null;
+            expect(simulate.numericDecision(fixtures.hasle, fixtures.commonwealth)).to.be.null;
         });
     });
     describe('NumericDestruction', function() {
@@ -32,15 +18,15 @@ describe('simulations', function() {
                 winner: {name: 'orion', ships:0},
                 looser: {name: 'commonwealth', ships:5}
             };
-            expect(simulate.numericDestruction(orion, commonwealth)).to.eql(result);
-            expect(simulate.numericDestruction(commonwealth, orion)).to.eql(result);
+            expect(simulate.numericDestruction(fixtures.orion, fixtures.commonwealth)).to.eql(result);
+            expect(simulate.numericDestruction(fixtures.commonwealth, fixtures.orion)).to.eql(result);
         });
         it('should identify equal fleets as a draw', () => {
             const result = {
                 draw1: {name: 'hasle', ships:0},
                 draw2: {name: 'commonwealth', ships:0}
             };
-            expect(simulate.numericDestruction(hasle, commonwealth)).to.eql(result);
+            expect(simulate.numericDestruction(fixtures.hasle, fixtures.commonwealth)).to.eql(result);
         });
     });
 });
