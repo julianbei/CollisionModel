921b41b7ef24a2cec11f52b01f5397ee9c3961d6
julianbei <julian@contentpass.de>
2017-09-12 19:45:02 +0200

    Add a more complex simulation including basic destruction

--- simple_model/index.js
+++ simple_model/index.js
@@ -4,6 +4,33 @@ function simulateNumericDecision(fleet1, fleet2){
   return null;
 }
 
+function simulateNumericDestruction(fleet1, fleet2){
+  // simulate destruction of ships
+  let ships = fleet1.ships - fleet2.ships;
+  // clone parameter objects for immutability
+  let combattant1 = {name: fleet1.name, ships};
+  let combattant2 = {name: fleet2.name, ships: ships*-1};
+
+  if (ships > 0) {
+    combattant2.ships = 0;
+    return {
+      winner: combattant1,
+      looser: combattant2
+    };
+  } else {
+    combattant1.ships = 0;
+    return {
+      winner: combattant2,
+      looser: combattant1
+    };
+  }
+  return {
+    draw1: combattant1,
+    draw2: combattant2
+  };
+}
+
+// Initialize test fleets
 const orion = {
   name: 'orion',
   ships: 295
@@ -19,6 +46,14 @@ const hasle = {
   ships: 300
 }
 
+// Simulations:
+
+//simulateNumericDecision
 console.log('simulateNumericDecision fight: 1\nwinner:', simulateNumericDecision(orion, commonwealth));
 console.log('simulateNumericDecision fight: 2\nwinner:', simulateNumericDecision(commonwealth, orion));
 console.log('simulateNumericDecision fight: 3\nwinner:', simulateNumericDecision(hasle, commonwealth));
+
+//simulateNumericDestruction
+console.log('simulateNumericDestruction fight: 1\nwinner:', simulateNumericDestruction(orion, commonwealth));
+console.log('simulateNumericDestruction fight: 2\nwinner:', simulateNumericDestruction(commonwealth, orion));
+console.log('simulateNumericDestruction fight: 3\nwinner:', simulateNumericDestruction(hasle, commonwealth));
