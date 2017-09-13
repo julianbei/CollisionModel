e820d0e1e34441d7fae45552f460a74cb8be62ed
julianbei <julian@contentpass.de>
2017-09-13 13:47:13 +0200

    Restructure tests to reflect simulation lib structure

--- simple_model/tests/fixtures/simple.js
+++ simple_model/tests/fixtures/simple.js
@@ -1,14 +1,16 @@
 module.exports = {
-    'orion': {
-        name: 'orion',
-        ships: 295
-    },
-    'commonwealth': {
-        name: 'commonwealth',
-        ships: 300
-    },
-    'hasle': {
-        name: 'hasle',
-        ships: 300
+    'numeric': {
+        'orion': {
+            name: 'orion',
+            ships: 295
+        },
+        'commonwealth': {
+            name: 'commonwealth',
+            ships: 300
+        },
+        'hasle': {
+            name: 'hasle',
+            ships: 300
+        }
     }
 };
