1ad0669eb3ac5dff8f017d4c63f17571c1d9095e
julianbei <julian@contentpass.de>
2017-09-13 19:18:03 +0200

    Major refactoring of SRP Model

--- simple_model/src/simulations/srp.js
+++ simple_model/src/simulations/srp.js
@@ -10,27 +10,56 @@
 //   }
 // };
 
-// How many will be killed by one
 const balancingSheet = {
+  combatClasses: [
+    'fighter',
+    'corvette',
+    'cruiser',
+    'dreadnought'
+  ],
   fighter: {
+    priorities:[
+      'fighter',
+      'dreadnought',
+      'cruiser',
+      'corvette'
+    ],
     fighter: 0.1,
     corvette: 0.02,
     cruiser: 0.04,
     dreadnought: 0.05
   },
   corvette: {
+    priorities:[
+      'fighter',
+      'corvette',
+      'cruiser',
+      'dreadnought'
+    ],
     fighter: 1,
     corvette: 0.2,
     cruiser: 0.2,
     dreadnought: 0.04
   },
   cruiser: {
+    priorities:[
+      'corvette',
+      'cruiser',
+      'dreadnought',
+      'fighter'
+    ],
     fighter: 0.5,
     corvette: 0.5,
     cruiser: 0.1,
     dreadnought: 0.2
   },
   dreadnought: {
+    priorities:[
+      'cruiser',
+      'dreadnought',
+      'corvette',
+      'fighter'
+    ],
     fighter: 0.10,
     corvette: 0.10,
     cruiser: 0.30,
@@ -51,7 +80,7 @@ function copyFleet(fleet){
     }
   };
 }
-// How many will be killed by one
+
 function dealDamage(atacker, target, ratio){
   let dealt = 0;
   if (atacker * ratio >= 1 && target !== 0) {
@@ -61,97 +90,44 @@ function dealDamage(atacker, target, ratio){
   return {atacker, dealt};
 }
 
-function fighterDamage(atacker){
+function fightPriority(atacker, shipClass){
   return function(ships){
     let result = {};
     let fight = {atacker, target: 0};
-    fight = dealDamage(fight.atacker, ships.fighter, balancingSheet.fighter.fighter);
-    result.fighter = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.dreadnought, balancingSheet.fighter.dreadnought);
-    result.dreadnought = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.cruiser, balancingSheet.fighter.cruiser);
-    result.cruiser = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.corvette, balancingSheet.fighter.corvette);
-    result.corvette = fight.dealt;
-    return result;
-  };
-}
-function corvetteDamage(atacker){
-  return function(ships){
-    let result = {};
-    let fight = {atacker, target: 0};
-    fight = dealDamage(fight.atacker, ships.fighter, balancingSheet.corvette.fighter);
-    result.fighter = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.corvette, balancingSheet.corvette.corvette);
-    result.corvette = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.cruiser, balancingSheet.corvette.cruiser);
-    result.cruiser = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.dreadnought, balancingSheet.corvette.dreadnought);
-    result.dreadnought = fight.dealt;
-    return result;
-  };
-}
-function cruiserDamage(atacker){
-  return function(ships){
-    let result = {};
-    let fight = {atacker, target: 0};
-    fight = dealDamage(fight.atacker, ships.corvette, balancingSheet.cruiser.corvette);
-    result.corvette = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.cruiser, balancingSheet.cruiser.cruiser);
-    result.cruiser = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.dreadnought, balancingSheet.cruiser.dreadnought);
-    result.dreadnought = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.fighter, balancingSheet.cruiser.fighter);
-    result.fighter = fight.dealt;
-    return result;
-  };
-}
-function dreadnoughtDamage(atacker){
-  return function(ships){
-    let result = {};
-    let fight = {atacker, target: 0};
-    fight = dealDamage(fight.atacker, ships.cruiser, balancingSheet.dreadnought.cruiser);
-    result.cruiser = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.dreadnought, balancingSheet.dreadnought.dreadnought);
-    result.dreadnought = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.corvette, balancingSheet.dreadnought.corvette);
-    result.corvette = fight.dealt;
-    fight = dealDamage(fight.atacker, ships.fighter, balancingSheet.dreadnought.fighter);
-    result.fighter = fight.dealt;
+    balancingSheet[shipClass].priorities.forEach((targetClass) => {
+      fight = dealDamage(fight.atacker, ships[targetClass], balancingSheet[shipClass][targetClass]);
+      result[targetClass] = fight.dealt;
+    });
     return result;
   };
 }
 
-function dealFleetDamage(atacker){
-  const fighter = fighterDamage(atacker.ships.fighter);
-  const corvette = corvetteDamage(atacker.ships.corvette);
-  const cruiser = cruiserDamage(atacker.ships.cruiser);
-  const dreadnought = dreadnoughtDamage(atacker.ships.dreadnought);
-  return function(fleet){
-    const result = [
-      fighter(fleet.ships),
-      corvette(fleet.ships),
-      cruiser(fleet.ships),
-      dreadnought(fleet.ships)
-    ];
-    return result.reduce((p, c) => {
+function dealFleetDamage(atacker, target){
+  atacker = copyFleet(atacker);
+  target = copyFleet(target);
+  return balancingSheet.combatClasses
+    .map(shipClass => fightPriority(atacker.ships[shipClass], shipClass))
+    .map(combat => combat(target.ships))
+    .reduce((p, c) => {
       return {
         fighter: Math.max(p.fighter - c.fighter, 0),
         corvette: Math.max(p.corvette - c.corvette, 0),
         cruiser: Math.max(p.cruiser - c.cruiser, 0),
         dreadnought: Math.max(p.dreadnought - c.dreadnought, 0)
       };
-    }, fleet.ships);
-  };
+    }, target.ships);
 }
 
-function fightFrame(pfleet1, pfleet2){
-  let dfleet1 = dealFleetDamage(copyFleet(pfleet1));
-  let dfleet2 = dealFleetDamage(copyFleet(pfleet2));
-
+function fightFrame(fleet1, fleet2){
   return {
-    fleet1: {name: pfleet1.name, ships:dfleet2(copyFleet(pfleet1))},
-    fleet2: {name: pfleet2.name, ships:dfleet1(copyFleet(pfleet2))}
+    fleet1: {
+      name: fleet1.name,
+      ships: dealFleetDamage(fleet2, fleet1)
+    },
+    fleet2: {
+      name: fleet2.name,
+      ships: dealFleetDamage(fleet1, fleet2)
+    }
   };
 }
 
