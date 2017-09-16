2e84187e5e6a8f3165719f5f89bd0af31ecb3eec
julianbei <julian@contentpass.de>
2017-09-16 12:26:33 +0200

    Add simple unit positioning system

--- naviagtion_tiles/src/index.js
+++ naviagtion_tiles/src/index.js
@@ -1,8 +1,39 @@
+class Unit {
+  constructor(id) {
+    this.id = id;
+    this.tile = undefined;
+  }
+  setPosition(tile){
+    this.tile = tile;
+  }
+  changePosition(coords){
+    const old = this.tile;
+    this.tile.system.setContent(this, coords);
+    old.removeContent(this);
+  }
+  removeFromMap(){
+    this.tile.removeContent(this);
+  }
+}
 
 class Tile {
-  constructor(x, y) {
+  constructor(x, y, system) {
     this.content = [];
     this.coordinates = {x,y};
+    this.system = system;
+  }
+  getContent(){
+    return this.content;
+  }
+  setContent(unit){
+    this.content.push(unit);
+    if(unit.tile) unit.tile.removeContent(unit);
+    unit.setPosition(this);
+  }
+  removeContent(unit){
+    this.content = this.content.filter( contended => (contended !== unit));
+    if(unit.tile === this) unit.tile = undefined;
+    return unit;
   }
 }
 
@@ -13,12 +44,18 @@ class Map {
     for(let x = 0; x < xAxisTiles; x++){
       xAxis[x] = new Array(yAxisTiles);
       for(let y = 0; y < yAxisTiles; y++){
-        xAxis[x][y] = new Tile(x,y);
+        xAxis[x][y] = new Tile(x,y, this);
       }
     }
-
     this.system = xAxis;
+  }
+  getContent(coords){
+    return this.system[coords.x][coords.y].getContent();
+  }
+  setContent(unit, coords){
+    const tile = this.system[coords.x][coords.y];
+    return tile.setContent(unit);
   }
 }
 
-module.exports = {Map};
+module.exports = {Map, Unit};
--- naviagtion_tiles/test/index.js
+++ naviagtion_tiles/test/index.js
@@ -1,12 +1,59 @@
 const expect = require('chai').expect;
 const navigation = require('./../src/index');
 
-describe('description', function() {
+describe('tile navigation system', function() {
   it('should create a 2D tile map', done => {
     const map = new navigation.Map(20,13);
     expect(map.system.length).to.eq(20);
     expect(map.system[0].length).to.eq(13);
     expect(map.system[0][12].constructor.name).to.eq('Tile');
+    done();
+  });
+  it('should create a Simple unit', done => {
+    const unit = new navigation.Unit('test1');
+    expect(unit.id).to.eq('test1');
+    done();
+  });
+
+  it('should add a Simple unit to map', done => {
+    const unit = new navigation.Unit('test1');
+    const map = new navigation.Map(20,13);
+    map.setContent(unit, {x:5,y:3});
+    expect(unit.id).to.eq('test1');
+    expect(unit.tile).to.eql(map.system[5][3]);
+    done();
+  });
+
+  it('should remove a Simple unit from map', done => {
+    const unit = new navigation.Unit('test1');
+    const map = new navigation.Map(20,13);
+    map.setContent(unit, {x:5,y:3});
+    map.system[5][3].removeContent(unit);
+    expect(unit.id).to.eq('test1');
+    expect(unit.tile).to.eq(undefined);
+    expect(map.getContent({x:5, y:3})).to.be.empty;
+    done();
+  });
+
+  it('should change the position of a Simple unit', done => {
+    const unit = new navigation.Unit('test1');
+    const map = new navigation.Map(20,13);
+    map.setContent(unit, {x:5,y:3});
+    unit.changePosition({x:2,y:5});
+    expect(unit.id).to.eq('test1');
+    expect(unit.tile).to.eq(map.system[2][5]);
+    expect(map.getContent({x:5, y:3})).to.be.empty;
+    done();
+  });
+
+  it('a unit should remove itself from map', done => {
+    const unit = new navigation.Unit('test1');
+    const map = new navigation.Map(20,13);
+    map.setContent(unit, {x:5,y:3});
+    unit.removeFromMap();
+    expect(unit.id).to.eq('test1');
+    expect(unit.tile).to.eq(undefined);
+    expect(map.getContent({x:5, y:3})).to.be.empty;
     done();
   });
 });
